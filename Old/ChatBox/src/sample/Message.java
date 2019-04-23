c2a0a9a9c00408a38e2f0857b50d9f971524f00a
Admin <jgarza145@islander.tamucc.edu>
2019-04-23 16:59:40 -0500

    display commit

---
+++ Old/ChatBox/src/sample/Message.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    protected static final long serialVersionUID = 3324002L;
+
+    public static final int WHOIS = 0, MESSAGE = 1, LOGOUT = 2;
+    private int type;
+    private String message;
+
+    // constructor
+    public Message(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    // getters
+    public int getType() {
+        return type;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
