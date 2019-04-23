89c36759cce4073c5d7d1016a3e355dcb8a00494
Admin <jgarza145@islander.tamucc.edu>
2019-04-23 17:09:51 -0500

    update

---
+++ ChatBot2/src/sample/Controller.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+
+public class Controller {
+    private String user;
+    private String message;
+    @FXML
+    private TextField txtUser, txtInput;
+
+    @FXML
+    private TextArea txtDisplay;
+
+
+    @FXML
+    private Button btnUser;
+
+
+    @FXML
+    private void sendMessage(ActionEvent event){
+        message = txtInput.getText();
+        txtDisplay.appendText(user + ": " + message + "\n\n");
+
+        txtInput.setText("");
+    }
+
+    @FXML
+    private void setUserName(ActionEvent event) {
+        user = txtUser.getText();
+        txtUser.setVisible(false);
+        btnUser.setVisible(false);
+    }
+}
